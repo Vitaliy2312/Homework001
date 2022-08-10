6436d136db99b955aa9b229a0098a4cb6e8c2919
«Vitaliy» <955550@list.ru>
2022-08-10 15:20:14 +0300

    Task_1.2

---
+++ Homework001.2/Program.cs
@@ -0,0 +1,18 @@
+﻿int a = 2;
+int b = 10;
+
+int max = 0;
+int min = a;
+
+if (a > max) max = a;
+if (b > max) max = b;
+
+Console.Write("max = ");
+Console.WriteLine(max);
+
+if (a < min) min = a;
+if (b < min) min = b;
+
+Console.Write("min = ");
+Console.WriteLine(min);
+
