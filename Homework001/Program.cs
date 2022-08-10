82cf636f01c684c9398b03c5798b0c888e7d7c44
«Vitaliy» <955550@list.ru>
2022-08-10 14:54:48 +0300

    gitignore

---
+++ Homework001/Program.cs
@@ -0,0 +1,17 @@
+﻿int a = 5;
+int b = 7;
+
+int max = 0;
+int min = a;
+
+if (a > max) max = a;
+if (b > max) max = b;
+
+Console.Write("max = ");
+Console.WriteLine(max);
+
+if (a < min) min = a;
+if (b < min) min = b;
+
+Console.Write("min = ");
+Console.WriteLine(min);
